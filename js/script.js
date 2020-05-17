0db030c79e04c3b82d21e7624e517e8beea4f363
m.zajacova90@gmail.com <Sestra90@>
2020-05-17 11:33:03 +0100

    added showPage function
    created the `showPage` function to hide all of the items in the list except for the 10 I want to show on the page

--- js/script.js
+++ js/script.js
@@ -5,19 +5,10 @@ FSJS project 2 - List Filter and Pagination
    
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
+const listItem = document.getElementsByClassName("student-item cf");
+//console.log(listItem);
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-
-
+const itemsPerPage = 10; 
 
 
 /*** 
@@ -35,16 +26,21 @@ FSJS project 2 - List Filter and Pagination
        "invoke" the function 
 ***/
 
+function showPage (list, page) {
+   const startIndex = (page * itemsPerPage) - itemsPerPage;
+   const endIndex = page * itemsPerPage;
+   for (i = 0; i < list.length; i++) {
+    if (i >= startIndex && i < endIndex) {
+     listItem[i].style.display = "";
+   } else {
+      listItem[i].style.display = "none";
+   }
+}
+}
 
 
+// Create the `appendPageLinks function` to generate, append, and add 
+// functionality to the pagination buttons.
+//function appendPageLinks () {}
+// 
 
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
-
-
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
