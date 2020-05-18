2466ab78c1f0a8cba2fd7b48df340b3b652bcead
m.zajacova90@gmail.com <Sestra90@>
2020-05-18 09:37:11 +0100

    work on the appendPageLinks function
    added loop, updated elements name to make the code more readable and clear

--- js/script.js
+++ js/script.js
@@ -5,70 +5,63 @@ FSJS project 2 - List Filter and Pagination
    
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
-const listItem = document.getElementsByClassName("student-item cf");
+const listItem = document.getElementsByClassName("student-item cf"); // list of students
 //console.log(listItem);
 
-const itemsPerPage = 10; 
+const itemsPerPage = 10; //a number of items displayed per page
 
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-
+// a function that will only show the ten items we want to show, the rest will be hidden
 function showPage (list, page) {
    const startIndex = (page * itemsPerPage) - itemsPerPage;
    const endIndex = page * itemsPerPage;
    for (i = 0; i < list.length; i++) {
     if (i >= startIndex && i < endIndex) {
-     listItem[i].style.display = "";
+      listItem[i].style.display = "";
    } else {
       listItem[i].style.display = "none";
+     }
    }
 }
-}
 
 
-// Create the `appendPageLinks function` to generate, append, and add 
-// functionality to the pagination buttons.
-
+// A function that will generate, append, and add functionality to the pagination buttons.
 
 function appendPageLinks (list) {
-  let pageDiv = document.createElement(".page");
-  let paginationDiv = document.createElement('div');
+  let pageDiv = document.querySelector("#page"); 
+  let paginationDiv = document.createElement("div"); //create a dynamic page structure - div, ul, li & a
   paginationDiv.className = "pagination";
-  let ul = document.createElement("ul");
   pageDiv.appendChild(paginationDiv);
-  paginationDiv.appendChild(ul);
+  let pageUL = document.createElement("ul");
+  paginationDiv.appendChild(pageUL);
   let pageNumber = Math.ceil(list.length / itemsPerPage);
   
   for (j = 0; j < pageNumber; j++) {
-   let li = document.createElement("li");
-   ul.appendChild(li);
-   let a = document.createElement("a");
-   li.appendChild(a);
+   let pageLI = document.createElement("li");
+   let pageA = document.createElement("a");
+   pageUL.appendChild(pageLI);
+   pageLI.appendChild(pageA);
+   pageA.href = "#";
+   pageA.innerHTML = pageNumber;
+   };
    
-
-
-  }
-
-
 }
 
 
 
+/*
+
+Pro Tip: The loop index can be helpful in setting the text of the pagination links.
+Add the active class name to the first pagination link initially.
+Add a “click” event listener to each A element. A loop can be helpful here.
+When a pagination link is clicked:
+The active class name should be removed from all pagination links. A loop can be helpful for this step.
+The active class name should be added to the link that was just clicked. The target property of the event object should be useful here.
+The function to show a page should be called, passing in as arguments, the global variable for the list items, and the page number that should be shown. The text content of the 
+A element that was just clicked can be helpful here.
+
+*/
 
 
+showPage(listItem, 1);
 
-li
-a href
